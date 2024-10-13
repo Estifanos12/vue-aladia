abc174da7e740526bce4f39bc2d10b4568a00e06
michael-gashawtena-superstruct <michael.gashawtena@dev.superstruct.tech>
2024-10-13 11:07:08 +0300

    Refactor Button component to include social button variant and image support

--- components/Atomic/Button.vue
+++ components/Atomic/Button.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 interface ButtonProps {
-  variant: "primary" | "golden";
+  variant: "primary" | "golden" | "social";
   label: string;
   onClick?: () => void;
   iconClass?: string;
   hasError?: boolean;
+  imgSrc?: string;
 }
 const props = defineProps<ButtonProps>();
 </script>
@@ -28,8 +29,14 @@ const props = defineProps<ButtonProps>();
       class="gradient absolute inset-0"
       bis_skin_checked="1"
     ></div>
-    <i data-v-df211d4a="" v-if="props.iconClass" :class="props.iconClass" class="me-2"></i> <div data-v-2e6163a4="" bis_skin_checked="1">{{ props.label }}</div> 
-    </div>
+    <i
+      data-v-df211d4a=""
+      v-if="props.iconClass"
+      :class="props.iconClass"
+      class="me-2"
+    ></i>
+    <div data-v-2e6163a4="" bis_skin_checked="1">{{ props.label }}</div>
+  </div>
 
   <!-- golden button -->
   <div
@@ -55,7 +62,20 @@ const props = defineProps<ButtonProps>();
       class="relative text-white z-10 flex items-center justify-center gap-2 group-hover:text-black"
       bis_skin_checked="1"
     >
-      <i data-v-df211d4a="" v-if="props.iconClass" :class="props.iconClass"></i> {{ props.label }}
+      <i data-v-df211d4a="" v-if="props.iconClass" :class="props.iconClass"></i>
+      {{ props.label }}
+    </div>
+  </div>
+
+  <!-- social button -->
+  <div
+    class="flex cursor-pointer items-center justify-center gap-4 rounded border border-white/20 py-1.5 shadow-[0px_3px_6px_#00000000] transition-all hover:bg-white/20 hover:shadow-[0px_3px_6px_#000000] active:scale-95"
+    bis_skin_checked="1"
+    v-if="props.variant === 'social'"
+  >
+    <img :src="props.imgSrc" class="h-6" />
+    <div class="w-40 text-white text-sm" bis_skin_checked="1">
+      {{ props.label }}
     </div>
   </div>
 </template>
